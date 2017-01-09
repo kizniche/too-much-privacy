3f84ad804fd17f625f7d310cf2a4a8df2096d6ac
Kyle Gabriel <KyleTGabriel@gmail.com>
2017-01-08 19:16:57 -0500

    test fix

--- chat.py
+++ chat.py
@@ -335,7 +335,7 @@ if __name__ == '__main__':
                                 c.output('[{time}] {nick} (*) {data}'.format(
                                     time=datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
                                     nick=each_data.split(' ', 1)[0],
-                                    data=tmp.decrypt_string('{data}-----END PGP MESSAGE-----'.format(data=each_data.split(' ', 1)[1]))), 'green')
+                                    data=tmp.decrypt_string('{data}-----END PGP MESSAGE-----'.format(data=each_data.split(' ', 1)[1]))).strip('\n'), 'green')
                             elif each_data != '\n':
                                 c.output('[{time}] (test) {data}'.format(
                                     time=datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
